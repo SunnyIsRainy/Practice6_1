30850879233af4c989040a6c6c9a764ec4715980
Sun_Rain_git <mlkkmn9900@kakao.com>
2022-08-04 16:48:45 +0900

    회원가입 창 제작

---
+++ app/src/main/java/com/example/practice6_1/SignUpActivity.kt
@@ -0,0 +1,21 @@
+package com.example.practice6_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+        setupEvent()
+        setValue()
+    }
+
+    override fun setupEvent() {
+    }
+
+    override fun setValue() {
+    }
+
+}
