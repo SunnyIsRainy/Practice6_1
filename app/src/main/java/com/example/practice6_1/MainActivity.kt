2d5f422e24145f6e725a4ff5150d19f63e461182
Sun_Rain_git <mlkkmn9900@kakao.com>
2022-08-04 18:18:17 +0900

    서버에서 Topic 데이터를 Topic형태에 맞춰 가져오기

--- app/src/main/java/com/example/practice6_1/MainActivity.kt
+++ app/src/main/java/com/example/practice6_1/MainActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.practice6_1.adapters.TopicAdapter
 import com.example.practice6_1.data.Topic
+import com.example.practice6_1.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -24,6 +26,34 @@ class MainActivity : BaseActivity() {
     override fun setValue() {
         mTopicAdapter = TopicAdapter(mContext, R.layout.topic_list_item, mTopicList)
         topicListView.adapter = mTopicAdapter
+
+        fun getTopicListFromServer() {
+
+            ServerUtil.getRequestTopicInfo(mContext, object : ServerUtil.Companion.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+
+                    val dataObj = jsonObj.getJSONObject("data")
+                    val topicsArr = dataObj.getJSONArray("topics")
+
+                    for (index in 0 until topicsArr.length()) {
+                        val topicsObj = topicsArr.getJSONObject(index)
+                        val topicsData = Topic.getTopicDataFromJson(topicsObj)
+
+                        mTopicList.add(topicsData)
+                    }
+
+                    runOnUiThread {
+                        mTopicAdapter.notifyDataSetChanged()
+                    }
+
+                }
+
+            })
+
+        }
+
+        getTopicListFromServer()
+
     }
 
 }
