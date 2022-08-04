588cc777e3ce53d92bd36dbdfbfc24609c60eaf4
Sun_Rain_git <mlkkmn9900@kakao.com>
2022-08-04 18:50:29 +0900

    PlashActivity 화면 제작

---
+++ app/src/main/java/com/example/practice6_1/PlashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.practice6_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlashActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plash)
+        setupEvent()
+        setValue()
+    }
+
+    override fun setupEvent() {
+    }
+
+    override fun setValue() {
+    }
+
+}
