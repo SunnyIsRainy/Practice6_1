503890a9680567ed05ce0f2c7aa4271da23d9f96
Sun_Rain_git <mlkkmn9900@kakao.com>
2022-08-04 16:51:44 +0900

    회원가입 서버 기능 입력

--- app/src/main/java/com/example/practice6_1/utils/ServerUtil.kt
+++ app/src/main/java/com/example/practice6_1/utils/ServerUtil.kt
@@ -37,6 +37,31 @@ class ServerUtil {
             })
         }
 
+        fun putRequestSignUp(email: String, password: String, nickname: String, handler: JsonResponseHandler?) {
+            val urlString = "${BASE_URL}/user"
+            val formData = FormBody.Builder()
+                .add("email", email)
+                .add("password", password)
+                .add("nick_name", nickname)
+                .build()
+            val request = Request.Builder()
+                .url(urlString)
+                .put(formData)
+                .build()
+            val client = OkHttpClient()
+            client.newCall(request).enqueue(object : Callback{
+                override fun onFailure(call: Call, e: IOException) {
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val bodyString = response.body!!.string()
+                    val jsonObj = JSONObject(bodyString)
+                    handler?.onResponse(jsonObj)
+                }
+
+            })
+        }
+
     }
 
 }
