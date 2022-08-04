eb275703d8ad794bd31e07e2743732d24806a9ed
Sun_Rain_git <mlkkmn9900@kakao.com>
2022-08-04 16:55:28 +0900

    회원가입 버튼 누를 시 회원가입 기능 활성화

--- app/src/main/java/com/example/practice6_1/SignUpActivity.kt
+++ app/src/main/java/com/example/practice6_1/SignUpActivity.kt
@@ -2,6 +2,10 @@ package com.example.practice6_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.practice6_1.utils.ServerUtil
+import kotlinx.android.synthetic.main.activity_sign_up.*
+import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
 
@@ -13,6 +17,30 @@ class SignUpActivity : BaseActivity() {
     }
 
     override fun setupEvent() {
+        signUpBtn.setOnClickListener {
+            val inputEmail = emailEdt.text.toString()
+            val inputPassword = passwordEdt.text.toString()
+            val inputNickname = nicknameEdt.text.toString()
+
+            ServerUtil.putRequestSignUp(inputEmail, inputPassword, inputNickname, object : ServerUtil.Companion.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+                    val code = jsonObj.getInt("code")
+                    if (code == 200) {
+                        val message = jsonObj.getString("message")
+                        runOnUiThread{
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                    else {
+                        val message = jsonObj.getString("message")
+                        runOnUiThread{
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+
+            })
+        }
     }
 
     override fun setValue() {
