fdb96f3fa1b7d26c3f55a28b8ca4d8a36d5aabef
Sun_Rain_git <mlkkmn9900@kakao.com>
2022-08-04 16:37:32 +0900

    ServerUtil 제작 및 로그인 기능 입력

---
+++ app/src/main/java/com/example/practice6_1/utils/ServerUtil.kt
@@ -0,0 +1,41 @@
+package com.example.practice6_1.utils
+
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
+
+class ServerUtil {
+
+    companion object {
+        interface JsonResponseHandler{
+            fun onResponse(jsonObj: JSONObject)
+        }
+    }
+
+    val BASE_URL = "http://54.180.52.26"
+
+    fun postRequestLogin(email: String, password: String, handler: JsonResponseHandler?) {
+        val urlString = "${BASE_URL}/user"
+        val formData = FormBody.Builder()
+            .add("email", email)
+            .add("password", password)
+            .build()
+        val request = Request.Builder()
+            .url(urlString)
+            .post(formData)
+            .build()
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object : Callback{
+            override fun onFailure(call: Call, e: IOException) {
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val bodyString = response.body!!.string()
+                val jsonObj = JSONObject(bodyString)
+                handler?.onResponse(jsonObj)
+            }
+
+        })
+    }
+
+}
