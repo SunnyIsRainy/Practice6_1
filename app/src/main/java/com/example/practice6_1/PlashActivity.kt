f8176c07a3399890c347c0f2f6fa0129d92de712
Sun_Rain_git <mlkkmn9900@kakao.com>
2022-08-04 18:54:41 +0900

    PlashActivity 지연 효과 적용

--- app/src/main/java/com/example/practice6_1/PlashActivity.kt
+++ app/src/main/java/com/example/practice6_1/PlashActivity.kt
@@ -1,7 +1,11 @@
 package com.example.practice6_1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.practice6_1.utils.ContextUtil
 
 class PlashActivity : BaseActivity() {
 
@@ -13,9 +17,24 @@ class PlashActivity : BaseActivity() {
     }
 
     override fun setupEvent() {
+
     }
 
     override fun setValue() {
+
+        val myHandler = Handler(Looper.getMainLooper())
+        myHandler.postDelayed({
+            if (ContextUtil.getToken(mContext) == null) {
+                val myIntent = Intent(mContext, LoginActivity::class.java)
+                startActivity(myIntent)
+            }
+            else {
+                val myIntent = Intent(mContext, MainActivity::class.java)
+                startActivity(myIntent)
+            }
+            finish()
+        }, 3000)
+
     }
 
 }
