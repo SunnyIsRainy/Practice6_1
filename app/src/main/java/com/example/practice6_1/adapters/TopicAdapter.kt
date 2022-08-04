e24ea94c9735eb86f486369866b5e8282a7f4f9d
Sun_Rain_git <mlkkmn9900@kakao.com>
2022-08-04 18:24:16 +0900

    가져온 Topic 데이터를 리스트뷰 위치에 알맞게 뿌리기

--- app/src/main/java/com/example/practice6_1/adapters/TopicAdapter.kt
+++ app/src/main/java/com/example/practice6_1/adapters/TopicAdapter.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.example.practice6_1.R
 import com.example.practice6_1.data.Topic
 
@@ -22,6 +25,13 @@ class TopicAdapter(
         }
 
         val row = tempRow!!
+
+        val data = mList[position]
+        val topicImg = row.findViewById<ImageView>(R.id.topicImg)
+        val topicTitle = row.findViewById<TextView>(R.id.topicTitleTxt)
+
+        topicTitle.text = data.title
+        Glide.with(mContext).load(data.imgUrl).into(topicImg)
 
         return row
     }
