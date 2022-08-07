a197a3f48aae181b63fd33301eb0cf8327d75be4
Sun_Rain_git <mlkkmn9900@kakao.com>
2022-08-07 14:52:48 +0900

    세부사항 엑티비티에 제목과 사진 가져오기

--- app/src/main/java/com/example/practice6_1/ViewTopicDetailActivity.kt
+++ app/src/main/java/com/example/practice6_1/ViewTopicDetailActivity.kt
@@ -1,9 +1,15 @@
 package com.example.practice6_1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.example.practice6_1.data.Topic
+import kotlinx.android.synthetic.main.activity_view_topic_detail.*
 
 class ViewTopicDetailActivity : BaseActivity() {
+
+    lateinit var mTopic: Topic
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +22,10 @@ class ViewTopicDetailActivity : BaseActivity() {
     }
 
     override fun setValue() {
+        mTopic = intent.getSerializableExtra("topic") as Topic
+
+        topicTitleTxt.text = mTopic.title
+        Glide.with(mContext).load(mTopic.imgUrl).into(topicImg)
     }
 
 }
