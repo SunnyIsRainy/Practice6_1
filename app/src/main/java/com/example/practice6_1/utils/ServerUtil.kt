8c0fa797a9d0ce2f718740d2e288400319d751d8
Sun_Rain_git <mlkkmn9900@kakao.com>
2022-08-09 15:27:33 +0900

    ServerUtil.kt에 투표기능 추가

--- app/src/main/java/com/example/practice6_1/utils/ServerUtil.kt
+++ app/src/main/java/com/example/practice6_1/utils/ServerUtil.kt
@@ -134,6 +134,30 @@ class ServerUtil {
             })
         }
 
+        fun postRequestVote(context: Context, sideId: Int, handler: JsonResponseHandler?) {
+            val urlString = "${BASE_URL}/user"
+            val formData = FormBody.Builder()
+                .add("side_id", sideId.toString())
+                .build()
+            val request = Request.Builder()
+                .url(urlString)
+                .post(formData)
+                .header("X-Http-Token", ContextUtil.getToken(context))
+                .build()
+            val client = OkHttpClient()
+            client.newCall(request).enqueue(object : Callback{
+                override fun onFailure(call: Call, e: IOException) {
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val bodyString = response.body!!.string()
+                    val jsonObj = JSONObject(bodyString)
+                    handler?.onResponse(jsonObj)
+                }
+
+            })
+        }
+
     }
 
 }
